1852e5d1def7d55b417c5213d677fd9aaa4b25fa
Eetu Rinkinen <eetu.rinkinen@gmail.com>
2025-03-23 14:27:02 +0200

    Add store view

--- ER-Stock Management-DataLibrary/Store.cs
+++ ER-Stock Management-DataLibrary/Store.cs
@@ -1,19 +1,17 @@
-﻿namespace ER_Stock_Management_DataLibrary
-{
-    public class Store
-    {
-        public required string Id { get; set; }
-        public required string Name { get; set; }
-        public required string City { get; set; }
-        public string? Address { get; set; }
-        public string? Supervisor { get; set; }
-        public string? Phone { get; set; }
-        public string? Email { get; set; }
-        public required List<Product> Products { get; set; }
+﻿using System.Diagnostics.CodeAnalysis;
 
-        public Store()
-        {
-            Products ??= [];
-        }
+namespace ER_Stock_Management_DataLibrary
+{
+    [method: SetsRequiredMembers]
+    public class Store(string name, string city, string? address, string? supervisor, string? phone, string? email)
+    {
+        public required string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string Name { get; set; } = name;
+        public required string City { get; set; } = city;
+        public string? Address { get; set; } = address;
+        public string? Supervisor { get; set; } = supervisor;
+        public string? Phone { get; set; } = phone;
+        public string? Email { get; set; } = email;
+        public required List<Product> Products { get; set; } = [];
     }
 }
