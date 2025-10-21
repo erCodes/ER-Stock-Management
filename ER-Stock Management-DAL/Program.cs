6c68c22eb3a3e1e20838b1f8df351a94ca450f67
Eetu Rinkinen <eetu.rinkinen@gmail.com>
2025-10-21 13:37:18 +0300

    oops

--- ER-Stock Management-DAL/Program.cs
+++ ER-Stock Management-DAL/Program.cs
@@ -5,7 +5,5 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "ER-Stock Management-DataAcessLayer");
-builder.Services.AddDbContext<Context>(options =>
-    options.UseSqlServer());
 
 app.Run();
