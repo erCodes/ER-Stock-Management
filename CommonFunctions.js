83ccafd068f09bcd89f84bc8cf3e6566480bb79a
Eetu Rinkinen <eetu.rinkinen@gmail.com>
2025-12-11 03:31:16 +0200

    Added translation method + index.html translated

--- CommonFunctions.js
+++ CommonFunctions.js
@@ -1,4 +1,4 @@
-/*Refactor path part*/
+﻿/*Refactor path part*/
 function Redirect(file, parameterKey = null, parameterValue = null) {
     let href = window.location.href;
     let path = href.substring(0, href.lastIndexOf('/')) + "/";
@@ -9,4 +9,51 @@ function Redirect(file, parameterKey = null, parameterValue = null) {
     }
 
     window.location.href = page;
+}
+
+
+function Translation(key) {
+    const lang = navigator.language;
+
+    let en = new Map();
+    en.set("AddStore", "Add store");
+    en.set("ProductCategories", "Product Categories");
+    en.set("StoresText", "Select Store to view its products");
+    en.set("StoresTextNotFound", "No stores found. Add new one to continue.");
+    en.set("", "");
+    en.set("", "");
+    en.set("", "");
+
+    let fi = new Map();
+    fi.set("AddStore", "Lisää kauppa");
+    fi.set("ProductCategories", "Tuotekategoriat");
+    fi.set("StoresText", "Valitse kauppa katsoaksesi sen tuotteita tai lisää uusi");
+    fi.set("StoresTextNotFound", "Kauppoja ei löytynyt. Lisää uusi jatkaaksesi.");
+    fi.set("", "");
+    fi.set("", "");
+
+
+    if (lang == "en") {
+        const enTranslation = en.get(key);
+
+        if (!enTranslation || enTranslation == null) {
+            return key + ": not found";
+        }
+
+        return enTranslation;
+    }
+
+    else if (lang == "fi") {
+        const fiTranslation = fi.get(key);
+
+        if (!fiTranslation || fiTranslation == null) {
+            return key + ": not found";
+        }
+
+        return fiTranslation;
+    }
+
+    else {
+        return "LANGUAGE ERROR";
+    }
 }
